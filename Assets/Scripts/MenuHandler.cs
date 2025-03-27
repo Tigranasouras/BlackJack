06baf1bb9f62885f1512c75ae637bc9579ada710
Daron <Daronbaltazar@gmail.com>
2025-03-27 18:37:42 -0500

    MenuHandler Script
    Menu Handler to handle scene transitions

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static Unity.VisualScripting.Member;
+
+
+public class MenuHandler : MonoBehaviour
+{
+    public AudioSource source;
+
+    public void goToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+    public void goToGameNoMusic()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Table");
+    }
+    private IEnumerator WaitForSoundAndTransition(string sceneName)
+    {
+
+        source.Play();
+        yield return new WaitForSeconds(source.clip.length);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+    public void goToGame()
+    {
+        StartCoroutine(WaitForSoundAndTransition("Table"));
+    }
+
+    public void restartGameScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void goToEndGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+    }
+
+}
