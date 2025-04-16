2193c9a1a9713ff883ab3826ad870f9d8b212fc8
Daron <Daronbaltazar@gmail.com>
2025-04-16 01:31:15 -0500

    FInished game
    Finished a playable version of the game.

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -14,4 +14,26 @@ public class ButtonHandler : MonoBehaviour
     {
         gameManager.onStand();
     }
+    public void doubleButton()
+    {
+        gameManager.onDouble();
+    }
+
+    public void nextHandButton()
+    {
+        gameManager.resetGame();
+    }
+
+    public void AddWagerButton1()
+    {
+        gameManager.addWager(1000);
+    }
+    public void AddWagerButton5()
+    {
+        gameManager.addWager(5000);
+    }
+    public void AddWagerButton10()
+    {
+        gameManager.addWager(10000);
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,29 +7,57 @@ public class GameManager : MonoBehaviour
 {
     public int playerScore = 0;
     public int dealerScore = 0;
+    public int cash = 1000000;
+    public int playerWager = 0;
     public TMPro.TextMeshProUGUI statusText;
     public CardManager cardManager;
     public TMPro.TextMeshProUGUI dealerCountText;
     public TMPro.TextMeshProUGUI playerCountText;
 
+    public TMPro.TextMeshProUGUI cashCountText;
+    public TMPro.TextMeshProUGUI wagerText;
+
+    bool handEnd = false;
+    bool handStart = false;
+    bool wagered = false;
+    bool wagerClose = false;
+
     public void onHit()
     {
-        cardManager.DealCard(true);
-        playerScore += DrawRandomCardScore(); // Score of dealt card
-
-        playerCountText.text = "Player Count: " + playerScore.ToString("00");
-
-
-        if (playerScore > 21)
+        if (!handEnd && wagered)
         {
-            statusText.text = "You busted!";
+            wagerClose = true;
+            handStart = true;
+            cardManager.DealCard(true);
+            playerScore += DrawRandomCardScore(); // Score of dealt card
+
+            playerCountText.text = "Player Count: " + playerScore.ToString("00");
+
+
+            if (playerScore > 21)
+            {
+                statusText.text = "You busted.";
+                // no need to update cash
+                UpdateCashText();
+                handEnd = true;
+                handStart = false;
+                playerWager = 0;
+                UpdateWagerText();
+
+
+            }
+        }
+        else {
+            statusText.text = "Wager or NextHand!";
         }
     }
 
     public void onStand()
     {
+        if (handStart && wagered)
+        {
 
-        while (dealerScore < 17)
+            while (dealerScore < 17)
         {
             cardManager.DealCard(false);
             dealerScore += DrawRandomCardScore(); //The score of the dealt card
@@ -39,27 +67,172 @@ public class GameManager : MonoBehaviour
         }
         if (dealerScore > 21 || playerScore > dealerScore)
         {
-            statusText.text = "You win!";
+            normalWin();
+            
         }
         else if (playerScore < dealerScore)
         {
-            statusText.text = "Dealer wins";
+            loose();
+            
 
         }
         else
         {
-            statusText.text = "Push!";
+            push();
+            
 
         }
-        // What happens next
+      }else
+        {
+            statusText.text = "Wager or NextHand!";
+        }
 
 
+    }
+
+
+    public void onDouble()
+    {
+        if (wagered && handStart && cash >= playerWager)
+        {
+            cash -= playerWager;
+            playerWager *= 2;
+            UpdateCashText();
+            UpdateWagerText();
+            wagerClose = true;
+            handStart = true;
+
+            cardManager.DealCard(true);
+            playerScore += DrawRandomCardScore();
+            playerCountText.text = "Player Count: " + playerScore.ToString("00");
+
+            if (playerScore > 21)
+            {
+                statusText.text = "You busted!";
+                handEnd = true;
+                handStart = false;
+                playerWager = 0;
+                UpdateWagerText();
+                UpdateCashText();
+            }
+            else
+            {
+                // Automatically stand if not busted
+                onStand();
+            }
+        }
+        else if (cash < playerWager)
+        {
+            statusText.text = "Not enough cash!";
+        }
+        else if (!wagered || !handStart)
+        {
+            statusText.text = "Can't double now!";
+        }
     }
 
 
     int DrawRandomCardScore()
     {
         return Random.Range(1, 11); // Random card dealing for testing
+    }
+
+    public void resetGame()
+    {
+        handEnd = false;
+        wagered = false;
+        wagerClose = false;
+        handStart = false;
+        playerScore = 0;
+        dealerScore = 0;
+        playerWager = 0;
+     
+
+
+        playerCountText.text = "Player Count: 00";
+        dealerCountText.text = "Dealer Count: 00";
+        cashCountText.text = cash.ToString("N0");
+        wagerText.text = "Wager: $" + playerWager.ToString("N0");
+
+        statusText.text = "Waitng on player!";
+
+    }
+
+    private void push()
+    {
+        statusText.text = "Push!";
+        cash += playerWager; // returns amount wagered
+        UpdateCashText();
+        handEnd = true;
+        handStart = false;
+        playerWager = 0;
+        UpdateWagerText();
+    }
+
+
+    private void normalWin()
+    {
+        statusText.text = "You win!";
+        cash += playerWager * 2;
+        UpdateCashText();
+        handEnd = true;
+        handStart = false;
+        playerWager = 0;
+        UpdateWagerText();
+
+    }
+
+    private void blackJackwin()
+    {
+        statusText.text = "BlackJack! You win!";
+        cash += Mathf.RoundToInt(playerWager * 2.5f);
+        UpdateCashText();
+        handEnd = true;
+        handStart = false;
+        playerWager = 0;
+        UpdateWagerText();
+
+    }
+
+    private void loose()
+    {
+        statusText.text = "Dealer wins!";
+        // no need to update cash
+        UpdateCashText();
+        handEnd = true;
+        handStart = false;
+        playerWager = 0;
+        UpdateWagerText();
+    }
+
+    private void UpdateCashText()
+    {
+        cashCountText.text = cash.ToString("N0");
+    }
+
+    private void UpdateWagerText()
+    {
+        wagerText.text = "Wager: $" + playerWager.ToString("N0");
+    }
+
+    public void addWager(int amount)
+    {
+        if (!wagerClose && cash >= amount)
+        {
+            playerWager += amount;
+            cash -= amount;
+            UpdateCashText();
+            UpdateWagerText();
+            if (!wagered) {
+                wagered = true;
+                statusText.text = "Waiting on Player!";
+            }
+
+        }
+        else if(cash < amount)
+        {
+            statusText.text = "Not enough Cash!";
+        }
     }
 
 }
